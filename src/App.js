ad49f355b0d9197fc253b796d47267167c4fae0a
jonmigreen <86339058+jonmigreen@users.noreply.github.com>
2023-04-08 16:31:18 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+onst Header = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <h1>{props.course}</h1>
+  )
 }
 
-export default App;
+const Content = (props) => {
+  return (
+    <>
+      <Part part1={props.parts[0].name} exercises1={props.parts[0].exercises} />
+      <Part part2={props.parts[1].name} exercises2={props.parts[1].exercises} />
+      <Part part3={props.parts[2].name} exercises3={props.parts[2].exercises} />
+    </>
+  )
+}
+
+const Total = (props) => {
+  let sum = props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises
+  console.log(sum)
+
+  return (
+    <p>
+      Number of exercises {sum}
+    </p>
+  )
+}
+
+const Part = (props) => {
+  console.log(props)
+  return (
+    <>
+      <p> {props.part1} {props.exercises1} </p>
+      <p> {props.part2} {props.exercises2} </p>
+      <p> {props.part3} {props.exercises3} </p>
+    </>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+  return(
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div >
+  )
+}
+
+export default App
